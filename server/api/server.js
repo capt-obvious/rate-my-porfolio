19f5a05ad2ac72d70fe3cd9d8b15418077b774af
Chrismis79 <chrismis79@gmail.com>
2020-01-02 13:59:28 -0600

    christine-fletcher

---
+++ server/api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware');
+const authRouter = require('../auth/auth-router');
+
+const userRouter = require('../users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+// server.use('/api/posts', authenticate, postsRouter);
+server.use('/api/users', userRouter);
+
+module.exports = server;
