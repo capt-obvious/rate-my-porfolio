b83f5a5ef0c57f1d7e00ce199e6459455a51b54f
David Francis <davidjayfrancis@gmail.com>
2020-01-02 20:46:27 -0800

    updated register

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 // component imports
-import Feed from "./components/Feed";
 import Home from "./pages/Home";
 import ProfilePage from "./pages/ProfilePage";
 import TradeForm from "./components/TradeForm.js";
