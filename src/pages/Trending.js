952fd2f0cc51be63dcbfa462e5ab44ac044915d7
Jake Cooley <jakeacooley@gmail.com>
2020-01-03 09:55:44 -0800

    Update Trending Page styling

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,26 +1,35 @@
 /* global fetch */
 import React from 'react'
+import styled from 'styled-components'
 import { ListGroup, ListGroupItem, Spinner } from 'reactstrap'
+import { TrendingContainer } from '../components/Containers'
 import { Header1 } from '../components/Headers'
 
 const SECRET_API_KEY = '7XJ0DGZYNBCMWHJ9'
+
+const FullListGroupItem = styled(ListGroupItem)`
+  width: 10em;
+`
 
 const Trending = () => {
   const [loading, setLoading] = React.useState(false)
   const [trending, setTrending] = React.useState([])
 
   const fetchTrendingStocks = (ticker) => {
-    return fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo`, {
+    return fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${SECRET_API_KEY}`, {
       method: 'GET'
     }).then((response) => response.json())
       .then((result) => {
+        console.log({ result })
         const quote = result['Global Quote']
-        const formattedTrend = {
-          symbol: quote['01. symbol'],
-          price: quote['05. price'],
-          change: quote['10. change percent']
+        if (quote) {
+          const formattedTrend = {
+            symbol: quote['01. symbol'],
+            price: quote['05. price'],
+            change: quote['10. change percent']
+          }
+          setTrending((prevTrending) => [ ...prevTrending, formattedTrend ])
         }
-        setTrending((prevTrending) => [ ...prevTrending, formattedTrend ])
       })
   }
 
@@ -28,19 +37,24 @@ const Trending = () => {
     setLoading(true)
     Promise.all([
       fetchTrendingStocks('MSFT'),
-      fetchTrendingStocks('APPL'),
+      fetchTrendingStocks('AAPL'),
       fetchTrendingStocks('TSLA'),
-      fetchTrendingStocks('FB'),
-      fetchTrendingStocks('NKE')
-    ]).then(() => setLoading(false))
+      fetchTrendingStocks('ADBE'),
+      fetchTrendingStocks('BAC')
+    ]).then(() => {
+      setLoading(false)
+    })
   }, [])
-
-  return loading ? <Spinner color='primary' /> : <>
+  console.log({ trending })
+  return loading ? <Spinner color='primary' /> : <TrendingContainer>
     <Header1>Top Trending Stocks</Header1>
-    <ListGroup>
-      {trending.map((trend, idx) => <ListGroupItem key={trend.symbol}>{`${trend.symbol} $${trend.price} ${trend.change}`}</ListGroupItem>)}
-    </ListGroup>
-  </>
+    {trending.length ? trending.map((trend) =>
+      <ListGroup key={trend.symbol} horizontal>
+        <FullListGroupItem color='info'>{trend.symbol}</FullListGroupItem>
+        <FullListGroupItem>{`$${trend.price}`}</FullListGroupItem>
+        <FullListGroupItem color={trend.change[0] === '-' ? 'danger' : 'success'}>{trend.change}</FullListGroupItem>
+      </ListGroup>) : <Header1>{'No Trending Stocks Right Now!'}</Header1>}
+  </TrendingContainer>
 }
 
 export default Trending
