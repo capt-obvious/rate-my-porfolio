61c293d68ff352bbde40b1f643a3fe707f44e6f9
David Francis <davidjayfrancis@gmail.com>
2020-01-02 17:01:10 -0800

    added Context for state management

---
+++ src/utils/Contexts.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const UserContext = React.createContext();
+
+export const UserContextProvider = props => {
+  const initialState = {
+    username: "",
+    name: "",
+    city: "",
+    avatar: "",
+    bio: "",
+    portfolio: [],
+    trades: []
+  };
+
+  const reducer = (state, action) => {
+    return {
+      ...state,
+      ...action
+    };
+  };
+
+  const [user, setUser] = React.useReducer(reducer, initialState);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
