49b169dad78800334cbb9a5eb7c41c33c9bd34bf
Eian Carter <eian@bricleir.com>
2020-01-02 20:04:57 -0800

    removed const for deleted comp

--- src/components/Welcome/WelcomeNav.js
+++ src/components/Welcome/WelcomeNav.js
@@ -1,29 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
 } from 'reactstrap';
 
 import Login from './Login';
 
+
 const WelcomeNav = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
-    <div>
+    <div className="welcomeNav">
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">rate-my-portfolio</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
               <Login />
           </Nav>
-        </Collapse>
       </Navbar>
     </div>
   );
