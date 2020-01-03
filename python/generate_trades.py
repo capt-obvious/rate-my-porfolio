aaedcafd9a4e3c29af124e41ec7f271bb3a7b515
Chris Maggio <chrismmaggio@gmail.com>
2020-01-02 16:29:07 -0800

    stock trades export to API endpoint

--- python/generate_trades.py
+++ python/generate_trades.py
@@ -2,6 +2,7 @@
 
 import yfinance as yf
 import pandas as pd
+import requests
 import random
 
 TICKERS = 'AAPL FB GOOG MSFT TSLA'
@@ -48,18 +49,37 @@ for i in range(250):
         df = df.sort_index()
         print(ticker, price, volume, action, portfolio)
 
-sql = {
-    'User'    : 'user',
-    'Pass'    : 'password',
-    'Address' : 'XXX.XXX.XXX.XXX',
-    'Port'    : 'XXXX',
-    'DB'      : 'database name',
-    'Table'   : 'table name'
-    }
+        for url in ['https://XXX.XXX.XXX.XXX']:
+            try:
+                response = requests.post(
+                                            url,
+                                            json={
+                                                    'ticker':ticker,
+                                                    'price':price,
+                                                    'volume':volume,
+                                                    'action':action}
+                                        )
+            except HTTPError as http_err:
+                print(f'HTTP error has occurred: {http_err}')
+            except Exception as err:
+                print(f'Non-HTTP error has occurred: {err}')
+            else:
+                print('Success.')
+
+
+
+# sql = {
+#    'User'    : 'user',
+#    'Pass'    : 'password',
+#    'Address' : 'XXX.XXX.XXX.XXX',
+#    'Port'    : 'XXXX',
+#    'DB'      : 'database name',
+#    'Table'   : 'table name'
+#    }
     
 
 # function to export dataframe to SQL database
-def exportDFtoSQL(dataframe: any, sql: Dict[str, str]):
+# def exportDFtoSQL(dataframe: any, sql: Dict[str, str]):
     
     #sql = {'User'   : 'user',
     #    'Pass'      : 'password',
@@ -69,28 +89,28 @@ def exportDFtoSQL(dataframe: any, sql: Dict[str, str]):
     #    'Table'     : 'table name'}
     
     # connect to SQL db
-    try:
-        engine = create_engine('mysql+mysqlconnector://'
-                +sql['User']+':'
-                +sql['Pass']+'@'
-                +sql['Address']+':'
-                +sql['Port']+'/'
-                +sql['DB'], echo=False)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            logging.warning(errorcode.ER_ACCESS_DENIED_ERROR)
-            print("Wrong user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            logging.warning(errorcode.ER_BAD_DB_ERROR)
-            print("Database does not exist")
-        else:
-            logging.warning(err)
-            print(err)
-    else:
-        logging.info('Connected to ' + sql['DB'])
-        print('Connected to ' + sql['DB'])
-
-    dataframe.to_sql(name=sql['Table'], con=engine, if_exists='replace')
+#    try:
+#        engine = create_engine('mysql+mysqlconnector://'
+#                +sql['User']+':'
+#                +sql['Pass']+'@'
+#                +sql['Address']+':'
+#                +sql['Port']+'/'
+#                +sql['DB'], echo=False)
+#    except mysql.connector.Error as err:
+#        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#            logging.warning(errorcode.ER_ACCESS_DENIED_ERROR)
+#            print("Wrong user name or password")
+#        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#            logging.warning(errorcode.ER_BAD_DB_ERROR)
+#            print("Database does not exist")
+#        else:
+#            logging.warning(err)
+#            print(err)
+#    else:
+#        logging.info('Connected to ' + sql['DB'])
+#        print('Connected to ' + sql['DB'])
+#
+#    dataframe.to_sql(name=sql['Table'], con=engine, if_exists='replace')
 
 # CREATE TABLE Errors (
 # id int not null AUTO_INCREMENT,
